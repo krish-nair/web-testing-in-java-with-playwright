2efdd702def6392a6079a38a54f0b8f92a866722
krish-nair <krish.nair101@gmail.com>
2024-08-02 13:33:31 +1200

    added a test with data source

--- web-testing-in-java-with-playwright/src/test/java/com/pw/PlaywrightTest.java
+++ web-testing-in-java-with-playwright/src/test/java/com/pw/PlaywrightTest.java
@@ -5,23 +5,33 @@ import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.imageio.IIOException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Properties;
 
 public class PlaywrightTest {
-
+    private Properties prop;
     private final Logger LOG = LoggerFactory.getLogger(PlaywrightTest.class);
     @Test
     public void playwrightTest(){
+        prop = readProperties();
         try(Playwright playwright = Playwright.create()) {
             BrowserType browserType = playwright.chromium();
-            Browser browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Browser browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(3000));
             Page page = browser.newPage();
-            LOG.info("Browser is launched and navigating to google website");
-            page.navigate("https://www.google.com");
+            LOG.info("Browser is launched...");
+            page.navigate(prop.getProperty("url"));
+            page.click("text = Add a new computer");
 
             String getPageTitle = page.title();
             LOG.info("Page title is '{}'", getPageTitle);
@@ -38,6 +48,38 @@ public class PlaywrightTest {
                 page.navigate("https://www.google.com");
                 page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshots/" + browserType.name() + ".png")));
             }
+        }
+    }
+
+    private Properties readProperties(){
+        try {
+            FileInputStream fis = new FileInputStream("src/main/resources/config/config.properties");
+            prop = new Properties();
+            prop.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException("File not found", e);
+        }
+        return prop;
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "text = Add a new computer",
+            "text = Add a New Computer",
+            "text = add a new computer"
+    })
+    public void playwrightTestWithParameters(String textSelector){
+        prop = readProperties();
+        try(Playwright playwright = Playwright.create()) {
+            BrowserType browserType = playwright.chromium();
+            Browser browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(3000));
+            Page page = browser.newPage();
+            LOG.info("Browser is launched...");
+            page.navigate(prop.getProperty("url"));
+            page.click(textSelector);
+
+            String getPageTitle = page.title();
+            LOG.info("Page title is '{}'", getPageTitle);
         }
     }
 }
